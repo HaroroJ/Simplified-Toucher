264cfc5311b897670ec7fd18a1f7dea3d4fe8b0a
JobsX <972739505@.qq.com>
2016-03-10 11:11:01 +0800

    Initial Commit

---
+++ simplifiedToucher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simplifiedToucher
+//
+//  Created by yons on 16/3/10.
+//  Copyright © 2016年 moreChinese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
